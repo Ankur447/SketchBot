204e4c1c3aa4bf556e9de2d9e26365c33fbc57a3
Varun Gujjar <varun@kenosis.in>
2023-06-01 20:10:10 +0530

    Path queue logic added
    Every time the user draws a new path it interrupted the robot drawing the previous path so had to create a  queuing system for every path that is drawn.

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,11 @@
 
+##########################################################################################
+##########################################################################################
+# DexArm Drawing Application
+# SVG / SVG Path to Gcode
+# Author : Varun Gujjar / Ronin Labs
+##########################################################################################
+##########################################################################################
 
 # from env import *
 from flask import Flask, request
@@ -7,9 +14,15 @@ from threading import Thread
 import requests
 from lib import SVG_GCode
 from lib.pydexarm import Dexarm
+import time
 
- 
-app = Flask(__name__)  
+from rq import Queue
+from rq.job import Job
+from worker import conn
+q = Queue(connection=conn)
+
+
+app = Flask(__name__)
 CORS(app)
 # cors = CORS(app, resource={
 #     r"/*":{
@@ -18,33 +31,33 @@ CORS(app)
 # })
 
 ##########################################################################################
-###### API Server Configuration
+# API Server Configuration
 ##########################################################################################
 
 host = '0.0.0.0'
-port=8251
-debug=False
+port = 8251
+debug = False
 
 ##########################################################################################
-###### Dex Arm Configuration
+# Dex Arm Configuration
 ##########################################################################################
 
 arm = None
 serial_port = "/dev/tty.usbmodem305A366030311"
 svg_gcode = SVG_GCode(
-                precision=10,
-                z_feedrate=3000, 
-                x_offset=-115, 
-                y_offset=230, 
-                z_surface_touch = 0, 
-                z_up_offset=5, 
-                bed_size_x=230, 
-                bed_size_y=150, 
-                longest_edge=230,
-                canvas_width=1024,
-                canvas_height=668,
-                verbose=True,             
-                fit_canvas=False)
+    precision=10,
+    z_feedrate=3000,
+    x_offset=-115,
+    y_offset=230,
+    z_surface_touch=0,
+    z_up_offset=5,
+    bed_size_x=230,
+    bed_size_y=150,
+    longest_edge=230,
+    canvas_width=1024,
+    canvas_height=668,
+    verbose=True,
+    fit_canvas=False)
 
 
 ##########################################################################################
@@ -55,7 +68,8 @@ def connect_arm():
     try:
         arm = Dexarm(port=serial_port)
         x, y, z, e, a, b, c = arm.get_current_position()
-        message = "DexArm connected x: {}, y: {}, z: {}, e: {}\na: {}, b: {}, c: {}".format(x, y, z, e, a, b, c)
+        message = "DexArm connected x: {}, y: {}, z: {}, e: {}\na: {}, b: {}, c: {}".format(
+            x, y, z, e, a, b, c)
         print(message)
         return message
     except:
@@ -76,17 +90,28 @@ def disconnect_arm():
 
 def plot_to_draw(data):
     global arm, port
+    time.sleep(0.2)
     connect_arm()
     for line in data:
         arm._send_cmd(f'{line}\r')
     disconnect_arm()
+    return True
 
 
+def plot_gcode(response):
+    try:
+        data = svg_gcode.path_to_gcode(
+            paths=response, plot_image=False, plot_file=True, gcode_path='output.gcode')
+        plot_to_draw(data)
+    except:
+        print("Something went wrong.")
+        return
 
 
 ##########################################################################################
-###### API Routes
+# API Routes
 ##########################################################################################
+
 
 @app.route('/')
 def index():
@@ -96,7 +121,15 @@ def index():
 @app.route('/move', methods=['POST'])
 def move():
     response = request.get_json()
-    #x y z e
+    global arm
+    connect_arm()
+    if arm is not None:
+        arm.move_to(x=response['x'], y=response['y'], z=response['z'])
+        x, y, z, e, a, b, c = arm.get_current_position()
+        position = {'x': x, 'y': y, 'z': z, 'e': e}
+        return position
+    else:
+        return 'No DexArm connected.'
 
 
 @app.route('/command/<string:command>', methods=['POST'])
@@ -117,24 +150,17 @@ def command(command):
         return '200 OK HTTPS.'
     else:
         return 'No DexArm connected.'
-    
+
 
 @app.route('/draw', methods=['POST'])
 def draw():
     response = request.get_json()
-    print(response)
-    global arm
-    connect_arm()
-    if arm is not None:
-        data = svg_gcode.path_to_gcode(paths=response, plot_image=False, plot_file=True, gcode_path='output.gcode')
-        plot_to_draw(data)
-        return '200 OK HTTPS.'
-    else:
-        return 'No DexArm connected.'        
-    
+    from flaskapp import plot_gcode
+    if len(response[0]['paths']) > 1:
+        job = q.enqueue(plot_gcode, response, result_ttl=2)
+        print(f'Started job with ID {job.get_id()}')
+    return '200 OK HTTPS.'
+
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0',port=5000,debug=True,threaded=True)
-
-
-
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
